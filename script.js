906a28bdff862c169ce4659e6f4628df852b5cdb
Kory Attleson <kory.attleson@gmail.com>
2021-07-30 14:33:29 -0500

    Created four functions to randomly generate destination, restaurant, mode of transport, and entertainment

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
 "use strict"
 
-// let age = 27;
-// console.log(age);
-
 // Declare function
 
-function dayTripGenerator(cities){
-    console.log(cities[0 || 1 || 2]);
-    return cities[0 || 1 || 2];
-}
-
-function newDayTripGenerator(newCity){
+function randomCityTripGenerator(cities){
     let citiesToVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
-    let userInput = prompt("Please enter 1, 2, or 3")
-    for(let i = 0; i < citiesToVisit.length; i++){
-        if(citiesToVisit[i] == userInput){
-            console.log("Your city to visit is")
-        }
-    }
+    let random = Math.floor(Math.random() *citiesToVisit.length);
+    let finalCity = citiesToVisit[random];
+    console.log(finalCity);
 }
 
+function randomRestaurantGenerator(food){
+    let placesToEat = ['McDonalds', 'Burger King', 'Pizza Hut']
+    let randomfood = Math.floor(Math.random() *placesToEat.length);
+    let finalPlaceToEat = placesToEat[randomfood];
+    console.log(finalPlaceToEat);
+}
+
+function randomModeOfTransport(vehicle){
+    let modeOfTransport = ['Car', 'Bus', 'Plane']
+    let randomTransport = Math.floor(Math.random() *modeOfTransport.length);
+    let finalModeOfTransport = modeOfTransport[randomTransport];
+    console.log(finalModeOfTransport);
+}
+
+function randomFormOfEntertainment(entertain){
+    let whatToDo = ['Casino', 'Clubbing', 'Beach']
+    let randomChoice = Math.floor(Math.random() *whatToDo.length);
+    let finalFormOfEntertainment = whatToDo[randomChoice];
+    console.log(finalFormOfEntertainment);
+}
+
+
+
+// create a function that randomly generates one random variable first
+// then do that four times
 
 // Setup Arguments to pass as parameters into functions
 
-let citiesToVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
+let cityTripVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
 let restaurantsToEatAt = ['McDonalds', 'Burger King', 'Pizza Hut']
 let modeOfTransportation = ['Car', 'Bus', 'Plane']
 let formOfEntertainment = ['Casino', 'Clubbing', 'Beach']
 
-// dayTripGenerator(citiesToVisit);
-let timeForVacation;
-newDayTripGenerator(timeForVacation);
+randomCityTripGenerator(cityTripVisit);
+randomRestaurantGenerator(cityTripVisit);
+randomModeOfTransport(cityTripVisit);
+randomFormOfEntertainment(cityTripVisit);
 
 // Execute function
 
