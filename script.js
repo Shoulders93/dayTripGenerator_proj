8a93e6602fadaefc032683d8fb7092bb6a63db47
Kory Attleson <kory.attleson@gmail.com>
2021-07-30 12:34:44 -0500

    created app.js and index.html in my project

---
+++ script.js
@@ -0,0 +1,21 @@
+// "use strict"
+
+let age = 27;
+console.log(age);
+
+// Declare function
+
+
+
+
+// Setup Arguments to pass as parameters into functions
+
+// let citiesToVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
+// let restaurantsToEatAt = ['McDonalds', 'Burger King', 'Pizza Hut']
+// let modeOfTransportation = ['Car', 'Bus', 'Plane']
+// let formOfEntertainment = ['Casino', 'Clubbing', 'Beach']
+
+// Execute function
+
+
+// Validate results
