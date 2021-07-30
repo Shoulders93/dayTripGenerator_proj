8ba2bf0b7934bfa3f8f30ab5f08636cf651915dd
Kory Attleson <kory.attleson@gmail.com>
2021-07-30 14:52:09 -0500

    Redid functions to pull in arrays instead of having arrays inside of function

--- script.js
+++ script.js
@@ -3,49 +3,52 @@
 // Declare function
 
 function randomCityTripGenerator(cities){
-    let citiesToVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
-    let random = Math.floor(Math.random() *citiesToVisit.length);
-    let finalCity = citiesToVisit[random];
+    let random = Math.floor(Math.random() *cities.length);
+    let finalCity = cities[random];
     console.log(finalCity);
 }
 
 function randomRestaurantGenerator(food){
-    let placesToEat = ['McDonalds', 'Burger King', 'Pizza Hut']
-    let randomfood = Math.floor(Math.random() *placesToEat.length);
-    let finalPlaceToEat = placesToEat[randomfood];
+    let randomfood = Math.floor(Math.random() *food.length);
+    let finalPlaceToEat = food[randomfood];
     console.log(finalPlaceToEat);
 }
 
 function randomModeOfTransport(vehicle){
-    let modeOfTransport = ['Car', 'Bus', 'Plane']
-    let randomTransport = Math.floor(Math.random() *modeOfTransport.length);
-    let finalModeOfTransport = modeOfTransport[randomTransport];
+    let randomTransport = Math.floor(Math.random() *vehicle.length);
+    let finalModeOfTransport = vehicle[randomTransport];
     console.log(finalModeOfTransport);
 }
 
 function randomFormOfEntertainment(entertain){
-    let whatToDo = ['Casino', 'Clubbing', 'Beach']
-    let randomChoice = Math.floor(Math.random() *whatToDo.length);
-    let finalFormOfEntertainment = whatToDo[randomChoice];
+    let randomChoice = Math.floor(Math.random() *entertain.length);
+    let finalFormOfEntertainment = entertain[randomChoice];
     console.log(finalFormOfEntertainment);
 }
-
-
 
 // create a function that randomly generates one random variable first
 // then do that four times
 
 // Setup Arguments to pass as parameters into functions
 
-let cityTripVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
-let restaurantsToEatAt = ['McDonalds', 'Burger King', 'Pizza Hut']
-let modeOfTransportation = ['Car', 'Bus', 'Plane']
-let formOfEntertainment = ['Casino', 'Clubbing', 'Beach']
+let citiesToVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
+let placesToEat = ['McDonalds', 'Burger King', 'Pizza Hut']
+let modeOfTransport = ['Car', 'Bus', 'Plane']
+let whatToDo = ['Casino', 'Clubbing', 'Beach']
 
-randomCityTripGenerator(cityTripVisit);
-randomRestaurantGenerator(cityTripVisit);
-randomModeOfTransport(cityTripVisit);
-randomFormOfEntertainment(cityTripVisit);
+randomCityTripGenerator(citiesToVisit);
+randomRestaurantGenerator(placesToEat);
+randomModeOfTransport(modeOfTransport);
+randomFormOfEntertainment(whatToDo);
+
+let choosenDestination;
+choosenDestination = prompt("Does this trip sound good?")
+    if(choosenDestination == "yes"){
+        console.log("Enjoy your trip");
+    }
+    else if(choosenDestination !== "yes"){
+        console.log("Refresh your screen");
+    }
 
 // Execute function
 
