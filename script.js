1475b335c4c057101be74e4236d2ca98023e9e1f
Kory Attleson <kory.attleson@gmail.com>
2021-07-30 13:52:57 -0500

    moved .js file from previous dayTripGenerator VSCode to correct .js file

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-// "use strict"
+"use strict"
 
-let age = 27;
-console.log(age);
+// let age = 27;
+// console.log(age);
 
 // Declare function
 
+function dayTripGenerator(cities){
+    console.log(cities[0 || 1 || 2]);
+    return cities[0 || 1 || 2];
+}
 
+function newDayTripGenerator(newCity){
+    let citiesToVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
+    let userInput = prompt("Please enter 1, 2, or 3")
+    for(let i = 0; i < citiesToVisit.length; i++){
+        if(citiesToVisit[i] == userInput){
+            console.log("Your city to visit is")
+        }
+    }
+}
 
 
 // Setup Arguments to pass as parameters into functions
 
-// let citiesToVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
-// let restaurantsToEatAt = ['McDonalds', 'Burger King', 'Pizza Hut']
-// let modeOfTransportation = ['Car', 'Bus', 'Plane']
-// let formOfEntertainment = ['Casino', 'Clubbing', 'Beach']
+let citiesToVisit = ['San Fransisco', 'New York City', 'Tampa Bay']
+let restaurantsToEatAt = ['McDonalds', 'Burger King', 'Pizza Hut']
+let modeOfTransportation = ['Car', 'Bus', 'Plane']
+let formOfEntertainment = ['Casino', 'Clubbing', 'Beach']
+
+// dayTripGenerator(citiesToVisit);
+let timeForVacation;
+newDayTripGenerator(timeForVacation);
 
 // Execute function
 
